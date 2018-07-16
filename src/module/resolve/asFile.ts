8b43e47c759629f22056702ffc0b8d16cf35f9b6
Eterion <Eterion@users.noreply.github.com>
2018-07-16 15:07:19 +0200

    Method for resolving module as file

---
+++ src/module/resolve/asFile.ts
@@ -0,0 +1,57 @@
+import name from 'module/name';
+import { basename } from 'path';
+import { Module, Path } from 'types';
+
+/**
+ * Returns true if module path should be ignored.
+ * @param {string} file Path to modules.
+ * @param {RegExp[]} regexp List of ignored regular expressions.
+ * @param {object} options Options.
+ */
+
+function isIgnored(file: string, regexp: RegExp[], options: Path): boolean {
+  return regexp
+    .map(
+      re =>
+        !new RegExp(`\.${options.moduleExtension}$`).test(file) || re.test(file)
+    )
+    .includes(true);
+}
+
+/**
+ * Returns list of regular expressions that should be ignored.
+ * @param {object} options Options.
+ */
+
+function ignoreList(options: Path): RegExp[] {
+  return [
+    new RegExp(`\.(?:d|spec|test)\.${options.moduleExtension}`),
+    new RegExp(`^${options.fileName}\.${options.fileExtension}$`),
+    ...(options.ignoreFiles
+      ? options.ignoreFiles.map(name => {
+          return new RegExp(
+            typeof name == 'string' && /^\//.test(name) && /\/$/.test(name)
+              ? name.substring(1, name.length - 1)
+              : `^${name}\.${options.moduleExtension}$`
+          );
+        })
+      : []),
+  ];
+}
+
+/**
+ * Returns module path resolved as file.
+ * @param {string} file Path to module.
+ * @param {object} options Options.
+ */
+
+export default function(file: string, options: Path): Module | null {
+  if (isIgnored(file, ignoreList(options), options)) {
+    return null;
+  }
+  const fileName = basename(file, '.' + options.moduleExtension);
+  return {
+    name: name(fileName),
+    path: `./${options.moduleExtensionInPath ? file : fileName}`,
+  };
+}
