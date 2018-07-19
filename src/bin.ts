ccceaedef915636f0f199b27719990fc568f72b6
Eterion <Eterion@users.noreply.github.com>
2018-07-19 06:39:16 +0200

    Updated bin script

--- src/bin.ts
+++ src/bin.ts
@@ -1,19 +1,15 @@
+import { Object } from 'core-js';
 import esmIndex from 'index';
 import config from 'params/config';
 import options from 'params/options';
-import { Config } from 'types';
 import filterObjectKeys from 'utils/filterObjectKeys';
 import * as yargs from 'yargs';
 
-esmIndex(<Config>(
-  filterObjectKeys(
-    yargs.options({ ...config, ...options }).argv,
-    ...[...Object.keys(config), ...Object.keys(options)]
-  )
-))
-  .then(callback => {
-    console.info('callback: ', callback);
-  })
-  .catch(err => {
-    console.error(err);
-  });
+const params = filterObjectKeys(
+  yargs.options(Object.assign({}, config, options)).argv,
+  ...[...Object.keys(config), ...Object.keys(options)]
+);
+
+esmIndex(params).catch(err => {
+  console.error(err);
+});
